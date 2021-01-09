733209603df7c53c1247abe8f49d44319b440fbf
AshleyCyberHub <acrandall22@byui.edu>
2021-01-08 23:27:33 -0600

    commit

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 window.addEventListener("load", event => {
     const updated = document.querySelector("#update");
     updated.textContent = document.lastModified;
-  })
   
+  var d = new Date();
+document.getElementById("updated").innerHTML = d;
+)}
