ba33e2d27ab07277eb24f0d54f533e13b8b5ed8f
AshleyCyberHub <acrandall22@byui.edu>
2021-02-05 13:22:59 -0600

    yes

--- lesson4/js/date.js
+++ lesson4/js/date.js
@@ -1,6 +1,12 @@
-function toggleMenu() {
-  document.getElementById("primaryNav").classList.toggle("hide");
-}
+
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
 
 let daynames = [
   "Sunday",
