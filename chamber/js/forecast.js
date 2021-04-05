16529d2f1ee385ad0fe83c75c877e2d2815a3ab7
AshleyCyberHub <acrandall22@byui.edu>
2021-04-05 17:54:42 -0500

    home page

--- chamber/js/forecast.js
+++ chamber/js/forecast.js
@@ -5,23 +5,23 @@ fetch(forecastapiURL)
     //console.log(jsObject); //to test
 const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-const fiveday = jsObject.list.filter((element) =>  element.dt_txt.includes("18:00:00")
+const threeday = jsObject.list.filter((element) =>  element.dt_txt.includes("18:00:00")
 );
 
 let day = 0;
 let i = 0;
 
-for (i = 0; i < fiveday.length; i++) {
-  let d = new Date(fiveday[i].dt_txt); 
+for (i = 0; i < threeday.length; i++) {
+  let d = new Date(threeday[i].dt_txt); 
 
   document.getElementById("dayofweek" + (day + 1)).textContent = days[d.getDay()];
-  document.getElementById("forecast" + (day + 1)).textContent = Math.round(fiveday[day].main.temp);
+  document.getElementById("forecast" + (day + 1)).textContent = Math.round(threeday[day].main.temp);
 
-  const imagesrc = "https://openweathermap.org/img/w/" + fiveday[day].weather[0].icon + ".png";
+  const imagesrc = "https://openweathermap.org/img/w/" + threeday[day].weather[0].icon + ".png";
 
   document.getElementById("imagesrc" + (day + 1)).textContent = imagesrc;
   document.getElementById("icon" + (day + 1)).setAttribute("src", imagesrc);
-  document.getElementById("icon" + (day + 1)).setAttribute("alt", fiveday[0].weather[0].description);
+  document.getElementById("icon" + (day + 1)).setAttribute("alt", threeday[0].weather[0].description);
 
   day++;
 }
