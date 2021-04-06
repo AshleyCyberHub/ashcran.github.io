431baa1eb8912222cdf5cbb4f5b3c575a1d81e8b
AshleyCyberHub <acrandall22@byui.edu>
2021-04-05 23:40:35 -0500

    update nav

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,32 +1,27 @@
 const requestdirectoryURL = 'https://ashleycyberhub.github.io/chamber/json/directory.json'; 
 
 fetch(requestdirectoryURL)
-.then(function (response) {
-   return response.json();
+     .then(function (response) {
+        return response.json();
 })
 .then(function (jsonObject) {
-   //console.table(jsonObject);  // temporary checking for valid response and data parsing
+   //console.table(jsonObject); } // temporary checking for valid response and data parsing
    const directory = jsonObject['directory'];
-   for (let i = 0; i < directory.length; i++) {
+   for (let i = 0; i < directory; i++) {
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
-      let image = document.createElement('img');
       let div1 = document.createElement('div');
       let div2 = document.createElement('div');
-      let div3 = document.createElement('div');
+      
       h2.textContent = directory[i].name + ' ';
       div1.setAttribute('class', 'address')
       div1.textContent = 'Address: ' + directory[i].address;
       div2.setAttribute('class', 'phonenumber')
       div2.textContent = 'Phone Number: ' + directory[i].phonenumber;
-      div3.setAttribute('class', 'website')
-      div3.textContent = 'Website: ' + directory[i].website;
-      image.setAttribute('src', directory[i].imageurl);
+      
       card.appendChild(h2);
       card.appendChild(div1);
       card.appendChild(div2);
-      card.appendChild(div3);
-      card.appendChild(image);
       document.querySelector('div.cards').appendChild(card);
    }
 });
